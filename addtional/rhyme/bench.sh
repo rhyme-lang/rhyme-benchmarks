4f78eeb7a0ef52a4dfa2a2046e635ffbd1b44ebe
Ran Guo <60993229+guo543@users.noreply.github.com>
2026-02-28 23:37:32 -0500

    Uncomment node command in bench.sh

--- addtional/rhyme/bench.sh
+++ addtional/rhyme/bench.sh
@@ -9,7 +9,7 @@ mkdir -p third-party/yyjson
 cp -v -r ../../rhyme/third-party/yyjson third-party/
 make -C third-party/yyjson
 
-#node --max-old-space-size=16384 rhyme-bench.js 6 1
+node --max-old-space-size=16384 rhyme-bench.js 6 1
 node rhyme-bench-c.js
 
 python3 bench.py
